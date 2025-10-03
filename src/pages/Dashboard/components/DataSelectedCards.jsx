0e19bbc573747458ec4ddc6346dadad80f004b04
MennaBashir <mennabashir00@gmail.com>
2025-10-03 16:10:26 +0300

    feat: update button functionality in DataSelectedCards and adjust navigation in SpecificHeader

--- src/pages/Dashboard/components/DataSelectedCards.jsx
+++ src/pages/Dashboard/components/DataSelectedCards.jsx
@@ -22,20 +22,18 @@ function DataSelectedCards() {
     }
     return (
         <div className='flex justify-between gap-4'>
-            {
-                selectedData?.activity &&
-                <button
-                    className="flex flex-col min-w-[300px] gap-1.5 rounded-2xl cursor-pointer text-[17px] p-5 
+
+            <button
+                className="flex flex-col min-w-[300px] gap-1.5 rounded-2xl cursor-pointer text-[17px] p-5 
              border border-[#948b8b]/40 
              bg-white/10 backdrop-blur-md shadow-md 
              transition duration-300 hover:bg-white/20 hover:shadow-lg"
-                    onClick={() => moveToSpecificStep("activity")}
-                >
-                    <h2 className="font-semibold text-white">Your Activity</h2>
-                    <p className="text-gray-200">{selectedData.activity}</p>
-                </button>
+                onClick={() => moveToSpecificStep("activity")}
+            >
+                <h2 className="font-semibold text-white">Your Activity</h2>
+                <p className="text-gray-200">{selectedData?.activity}</p>
+            </button>
 
-            }
             <button className='flex flex-col min-w-[300px] gap-1.5 rounded-2xl cursor-pointer text-[17px] p-5 
              border border-[#948b8b]/40 
              bg-white/10 backdrop-blur-md shadow-md 
